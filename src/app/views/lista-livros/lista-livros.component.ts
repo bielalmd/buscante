cd9bc859d4f3b97d45696d7d1a749ea2f1a66900
bielalmd <austbiel@gmail.com>
2023-01-21 19:50:53 -0300

    about rxJs

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -13,6 +13,9 @@ export class ListaLivrosComponent {
   constructor(private service: LivroService) {}
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca);
+    this.service
+      .buscar(this.campoBusca)
+      .subscribe((retornoAPI) => console.log(retornoAPI)
+      )
   }
 }
