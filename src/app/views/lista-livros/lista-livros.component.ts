883484a526ab859c43e9d8bcfd1825366bbe54e9
bielalmd <austbiel@gmail.com>
2023-01-22 22:34:45 -0300

    unsubscribe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
 import { LivroService } from "src/app/service/livro.service";
 
 @Component({
@@ -6,21 +7,24 @@ import { LivroService } from "src/app/service/livro.service";
   templateUrl: "./lista-livros.component.html",
   styleUrls: ["./lista-livros.component.css"],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy{
   listaLivros: [];
   campoBusca: string = "";
+  subscription: Subscription
 
   constructor(private service: LivroService) {}
 
   buscarLivros() {
-    this.service
+    this.subscription = this.service
       .buscar(this.campoBusca)
       .subscribe({
         next: retornoAPI => console.log(retornoAPI),
         error: erro => console.log(erro),
         complete: () => console.log('Observable complete')
-        
-        
       }) 
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
   }
 }
